6f233a740fc1233b4a83f1864ff7bdf9073a5d0e
zeshan <developer4.grafdom@gmail.com>
2025-11-03 16:33:17 +0500

    feat: init

--- backend/src/common/supabase/supabase.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { SupabaseService } from './supabase.service';
-
-@Global()
-@Module({
-  providers: [SupabaseService],
-  exports: [SupabaseService],
-})
-export class SupabaseModule {}
